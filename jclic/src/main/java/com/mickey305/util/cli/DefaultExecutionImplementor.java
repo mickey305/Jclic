97843ea8bf418c49f0b769f9a5b316d8f1208e86
K.Misaki <pro.mickey.12091it@gmail.com>
2017-05-27 16:11:17 +0900

    added implement class

---
+++ jclic/src/main/java/com/mickey305/util/cli/DefaultExecutionImplementor.java
@@ -0,0 +1,49 @@
+package com.mickey305.util.cli;
+
+import com.mickey305.util.cli.model.Arguments;
+import com.mickey305.util.cli.runtime.Executor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mickey305.util.cli.TerminalCommandUtils.generatePipeByArguments;
+
+/**
+ * Created by K.Misaki on 2017/05/27.
+ *
+ */
+public class DefaultExecutionImplementor {
+    public static <C extends TerminalCommand> int impl(C instance) {
+        int status;
+        List<Arguments> argumentsList = new ArrayList<>();
+        Executor executor = new Executor();
+
+        List<TerminalCommand> pipeCommands = instance.getPipeCommands();
+        Arguments args = instance.getArgs();
+
+        argumentsList.add(args);
+
+        if (!pipeCommands.isEmpty()) {
+            // pipe logic
+            pipeCommands.forEach(cmd -> argumentsList.add(cmd.getArgs()));
+            String cmdSentence = generatePipeByArguments(argumentsList);
+            status = executor.executeRuntime(cmdSentence, process -> {
+                instance.createPid(process);
+                instance.createResultSet(process);
+                return process.exitValue();
+            });
+        } else {
+            status = executor.executeRuntime(args, process -> {
+                instance.createPid(process);
+                instance.createResultSet(process);
+                return process.exitValue();
+            });
+        }
+
+        Receiver receiver = instance.getReceiver();
+        if (receiver != null)
+            receiver.action(argumentsList, status, instance.getResultSet());
+
+        return status;
+    }
+}
--- jclic/src/main/java/com/mickey305/util/cli/commands/GrepCommand.java
+++ jclic/src/main/java/com/mickey305/util/cli/commands/GrepCommand.java
@@ -1,15 +1,8 @@
 package com.mickey305.util.cli.commands;
 
-import com.mickey305.util.cli.Receiver;
+import com.mickey305.util.cli.DefaultExecutionImplementor;
 import com.mickey305.util.cli.TerminalCommand;
 import com.mickey305.util.cli.TerminalCommandBuilder;
-import com.mickey305.util.cli.model.Arguments;
-import com.mickey305.util.cli.runtime.Executor;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.mickey305.util.cli.TerminalCommandUtils.generatePipeByArguments;
 
 /**
  * Created by K.Misaki on 2017/05/04.
@@ -28,36 +21,6 @@ public class GrepCommand extends TerminalCommand {
 
     @Override
     protected int executeLogic() {
-        int status;
-        List<Arguments> argumentsList = new ArrayList<>();
-        Executor executor = new Executor();
-
-        List<TerminalCommand> pipeCommands = getPipeCommands();
-        Arguments args = getArgs();
-
-        argumentsList.add(args);
-
-        if (!pipeCommands.isEmpty()) {
-            // pipe logic
-            pipeCommands.forEach(cmd -> argumentsList.add(cmd.getArgs()));
-            String cmdSentence = generatePipeByArguments(argumentsList);
-            status = executor.executeRuntime(cmdSentence, process -> {
-                createPid(process);
-                createResultSet(process);
-                return process.exitValue();
-            });
-        } else {
-            status = executor.executeRuntime(args, process -> {
-                createPid(process);
-                createResultSet(process);
-                return process.exitValue();
-            });
-        }
-
-        Receiver receiver = getReceiver();
-        if (receiver != null)
-            receiver.action(argumentsList, status, getResultSet());
-
-        return status;
+        return DefaultExecutionImplementor.impl(this);
     }
 }

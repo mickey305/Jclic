5f89b9fd7e25795ae95ecd4549c0e21aacf0340f
K.Misaki <pro.mickey.12091it@gmail.com>
2017-07-08 13:50:07 +0900

    added a sampling logic

--- jclic/src/main/java/com/mickey305/util/cli/DefaultExecutionImplementor.java
+++ jclic/src/main/java/com/mickey305/util/cli/DefaultExecutionImplementor.java
@@ -5,6 +5,8 @@ import com.mickey305.util.cli.runtime.Executor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 import static com.mickey305.util.cli.TerminalCommandUtils.generatePipeByArguments;
 
@@ -13,6 +15,25 @@ import static com.mickey305.util.cli.TerminalCommandUtils.generatePipeByArgument
  *
  */
 public class DefaultExecutionImplementor {
+    private static Stack<Process> executedProcessBuf;
+
+    // TODO #1 : test injected method
+    private static Stack<Process> getExecutedProcessBuf() {
+        if (executedProcessBuf == null)
+            executedProcessBuf = new Stack<>();
+        return executedProcessBuf;
+    }
+
+    // TODO #2 : test injected method
+    private static void killAllProcess() {
+        getExecutedProcessBuf().forEach(p -> { if (p.isAlive()) p.destroy(); });
+    }
+
+    // TODO #3 : test injected method
+    private static List<Process> killAllProcessForcibly() {
+        return getExecutedProcessBuf().stream().map(Process::destroyForcibly).collect(Collectors.toList());
+    }
+
     public static <C extends TerminalCommand> int impl(C instance) {
         int status;
         List<Arguments> argumentsList = new ArrayList<>();
@@ -28,12 +49,18 @@ public class DefaultExecutionImplementor {
             pipeCommands.forEach(cmd -> argumentsList.add(cmd.getArgs()));
             String cmdSentence = generatePipeByArguments(argumentsList);
             status = executor.executeRuntime(cmdSentence, process -> {
+                /* todo : stack a executed process
+                getExecutedProcessBuf().push(process);
+                */
                 instance.createPid(process);
                 instance.createResultSet(process);
                 return process.exitValue();
             });
         } else {
             status = executor.executeRuntime(args, process -> {
+                /* todo : stack a executed process
+                getExecutedProcessBuf().push(process);
+                */
                 instance.createPid(process);
                 instance.createResultSet(process);
                 return process.exitValue();
